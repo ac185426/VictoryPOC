435c4f32c688fdf40939c0d05ce3411b508dd4fb
Andres <aa185467@ncr.com>
2022-02-18 11:16:29 -0500

    custom labels not showing up

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import "./App.css";
-import { VictoryBar, VictoryChart, VictoryAxis } from "victory";
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryAxis,
+  VictoryTheme,
+  VictoryLabel,
+} from "victory";
 import data from "./Data/data";
 import CardComponent from "./Components/Card";
+import { Typography } from "@mui/material";
+import props, { Children } from "react";
 
-export const App = () => {
+export const App = (props: any) => {
   return (
     <VictoryChart
+      // adding the material theme provided with Victory
+      theme={VictoryTheme.material}
       // domainPadding will add space to each side of VictoryBar to
       // prevent it from overlapping the axis
-      domainPadding={20}
+      domainPadding={{ x: 20, y: 20 }}
     >
       <VictoryAxis
         // tickValues specifies both the number of ticks and where
@@ -21,7 +31,26 @@ export const App = () => {
         // tickFormat specifies how ticks should be displayed
         tickFormat={(x) => `$${x / 1000}k`}
       />
-      <VictoryBar data={data} x="quarter" y="earnings" />
+      <VictoryBar
+        //override styles
+        style={{ data: { fill: "#c43a31" } }}
+        //Data access through prop
+        data={data}
+        // data accessor for x values
+        x="quarter"
+        // data accessor for y values
+        y="earnings"
+        //labels
+        labels={({ datum }) => `y: ${datum.y}`}
+        labelComponent={
+          // <VictoryLabel
+          //   dy={-20}
+          //   backgroundStyle={{ fill: "tomato", opacity: 0.6 }}
+          //   backgroundPadding={{ bottom: 5, top: 5 }}
+          // ></VictoryLabel>
+          <Typography {...props}></Typography>
+        }
+      />
     </VictoryChart>
   );
 
