47f6552d6c86b59687305bb6b749e319e548a1fe
Andres <aa185467@ncr.com>
2022-03-08 17:12:14 -0500

    changes might be a duplicate

--- src/Components/BarCard.tsx
+++ src/Components/BarCard.tsx
@@ -1,8 +1,8 @@
-import { Box, Card, CardContent, Typography } from '@mui/material';
-import useCard from './useCard';
-import { useState, useEffect } from 'react';
-import darkTheme from '../Themes/darkTheme';
-import lightTheme from '../Themes/lightTheme';
+import { Button, Box, Card, CardContent, Typography } from "@mui/material";
+import useCard from "./useCard";
+import { useState, useEffect } from "react";
+import darkTheme from "../Themes/darkTheme";
+import lightTheme from "../Themes/lightTheme";
 import {
   VictoryBar,
   VictoryChart,
@@ -12,11 +12,10 @@ import {
   Bar,
   VictoryLabel,
   VictoryPortal,
-  VictoryContainer,
-} from 'victory';
-import data from '../Data/data';
-import { CustomText } from './CustomText';
-import { VictoryAccessibleGroup } from 'victory-core';
+} from "victory";
+import data from "../Data/data";
+import { CustomText } from "./CustomText";
+import { VictoryAccessibleGroup } from "victory-core";
 
 const CardComponent = () => {
   const {
@@ -30,36 +29,60 @@ const CardComponent = () => {
     subTextTheme,
   } = useCard();
 
-  const [showGrid, setShowGrid] = useState(true);
+  const [showGrid, setShowGrid] = useState(false);
 
+  useEffect(() => {});
+
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {});
+
+  //Not optimal
   useEffect(() => {
     const updateGrid = () => {
-      console.log('Show Grid: ', showGrid);
       if (showGrid) {
-        if (lob === 'Light') {
-          lightTheme.axis.style.grid.stroke = 'black';
-        } else if (lob === 'Dark') {
-          darkTheme.axis.style.grid.stroke = 'white';
+        if (lob === "Light") {
+          lightTheme.axis.style.grid.stroke = "black";
+        } else if (lob === "Dark") {
+          darkTheme.axis.style.grid.stroke = "white";
         } else {
-          lightTheme.axis.style.grid.stroke = '';
-          darkTheme.axis.style.grid.stroke = '';
+          lightTheme.axis.style.grid.stroke = "";
+          darkTheme.axis.style.grid.stroke = "";
         }
       } else {
-        lightTheme.axis.style.grid.stroke = '';
-        darkTheme.axis.style.grid.stroke = '';
+        lightTheme.axis.style.grid.stroke = "";
+        darkTheme.axis.style.grid.stroke = "";
       }
     };
     updateGrid();
-  }, [showGrid, chartTheme, lob, DarkStyles, LightStyles, background]);
+    return () => {
+      lightTheme.axis.style.grid.stroke = "";
+      darkTheme.axis.style.grid.stroke = "";
+    };
+  }, [showGrid]);
 
-  const hideGrid = () => {
+  const toggleGrid = () => {
+    // console.log("Button was clicked");
+    // if (showGrid === false) {
+    //   setShowGrid(true);
+    // }
     setShowGrid(!showGrid);
   };
 
   return (
-    <Card sx={{ display: 'flex', flexDirection: 'row', backgroundColor: background, height: 500 }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <CardContent sx={{ flex: '1 0 auto' }}>
+    <Card
+      aria-label="main-container"
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        backgroundColor: background,
+      }}
+    >
+      <Box
+        aria-label="content-container"
+        sx={{ display: "flex", flexDirection: "column" }}
+      >
+        <CardContent sx={{ flex: "1 0 auto" }}>
           {/* Header & Subheader */}
           <Typography component="div" variant="h5" sx={{ color: textTheme }}>
             Victory POC
@@ -67,66 +90,37 @@ const CardComponent = () => {
           <Typography variant="subtitle1" sx={{ color: subTextTheme }}>
             {lob}
           </Typography>
-          <Typography sx={{ color: textTheme, maxWidth: 400, mt: 14 }}>
-            Chart displays the average cost customers pay when eating at these resturants. At
-            Chipotle customers average $5, at Mad Mex customers average $4, at Starbucks customers
-            average $6, at Brusters customers average $3, and at Jinya customers average $7.
-          </Typography>
         </CardContent>
-        <Box sx={{ display: 'block', alignItems: 'center', pl: 1, pb: 1 }}>
+        <Box sx={{ display: "block", alignItems: "center", pl: 1, pb: 1 }}>
           {/* Theme Buttons
            *This needs to be dynamic with just one render
            */}
 
-          {lob === 'Light' && (
-            <div>
-              <LightStyles variant="outlined" onClick={themeHandler}>
-                Change Theme
-              </LightStyles>
-              <LightStyles variant="outlined" onClick={hideGrid} sx={{ ml: 1, minWidth: '10em' }}>
-                {showGrid ? 'Show Grid' : 'Hide Grid'}
-              </LightStyles>
-            </div>
-          )}
-
-          {lob === 'Dark' && (
-            <div>
-              <DarkStyles variant="outlined" onClick={themeHandler}>
-                Change Theme
-              </DarkStyles>
-              <DarkStyles variant="outlined" onClick={hideGrid} sx={{ ml: 1, minWidth: '10em' }}>
-                {showGrid ? 'Show Grid' : 'Hide Grid'}
-              </DarkStyles>
-            </div>
-          )}
+          <div>
+            <Button variant="outlined" onClick={themeHandler}>
+              Change Theme
+            </Button>
+          </div>
         </Box>
       </Box>
-      <Box sx={{ color: '#EBEBF599', height: 400, width: 800, mt: 6 }}>
+      <Box aria-label="chart-container" sx={{ color: "#EBEBF599" }}>
         <VictoryAccessibleGroup aria-label="POC of the Victory Chart Library">
-          <VictoryChart
-            domainPadding={30}
-            theme={chartTheme}
-            padding={{ left: 50, top: 45, bottom: 60, right: 0 }}
-          >
+          <VictoryChart domainPadding={50} theme={chartTheme}>
             <VictoryLabel
               text="Profit per Company"
               textAnchor="middle"
-              x={260}
+              x={225}
               y={30}
               style={[{ fill: textTheme, fontSize: 20 }]}
             />
             <VictoryAxis
-              label="Restaurants"
-              axisLabelComponent={
-                <VictoryLabel dy={20} style={[{ fill: textTheme, fontSize: 16 }]} />
-              }
+            // tickValues specifies both the number of ticks and where
+            // they are placed on the axis
+            //tickValues={data.label}
+            //tickFormat={({ datum }) => datum.tick}
             />
             <VictoryAxis
               dependentAxis
-              label="Average Customer Cost"
-              axisLabelComponent={
-                <VictoryLabel dy={-10} style={[{ fill: textTheme, fontSize: 16 }]} />
-              }
               // tickFormat specifies how ticks should be displayed
               tickFormat={(x) => `$${x}`}
             />
@@ -136,9 +130,10 @@ const CardComponent = () => {
               labels={({ datum }) => datum.label}
               labelComponent={<VictoryTooltip />}
               //labelComponent={<CustomText />}
+
               //accessability which allows tab key through all data bars
 
-              dataComponent={<Bar tabIndex={0} ariaLabel={({ datum }) => `${datum.x}`} />}
+              dataComponent={<Bar tabIndex={0} />}
               style={{
                 data: {
                   fill: ({ datum }) => datum.fill,
@@ -150,8 +145,8 @@ const CardComponent = () => {
               //     //eventKey: 'esc',
               //     eventHandlers: {
               //       //remove default label events
-              //       onMouseOver: () => {},
-              //       onMouseOut: () => {},
+              //       //onMouseOver: () => {},
+              //       //onMouseOut: () => {},
 
               //       //add toggle for tooltip
               //       onClick: () => {
@@ -170,10 +165,10 @@ const CardComponent = () => {
               // ]}
             />
             <VictoryLabel
-              text=""
+              text="description of chart"
               textAnchor="middle"
               x={225}
-              y={300}
+              y={0}
               dx={0}
               dy={0}
               style={[{ fill: textTheme, fontSize: 20 }]}
--- src/Components/useCard.ts
+++ src/Components/useCard.ts
@@ -1,57 +1,58 @@
-import lightTheme from '../Themes/lightTheme';
-import darkTheme from '../Themes/darkTheme';
-import { useState } from 'react';
-import { styled } from '@mui/material/styles';
-import { Button, ButtonProps } from '@mui/material';
+import lightTheme from "../Themes/lightTheme";
+import darkTheme from "../Themes/darkTheme";
+import { useState } from "react";
+import { styled } from "@mui/material/styles";
+import { Button, ButtonProps } from "@mui/material";
 
 const useCard = () => {
   const [chartTheme, setChartTheme] = useState(lightTheme);
-  const [lob, setLob] = useState('Light');
-  const [background, setBackground] = useState('#ffffff');
-  const [textTheme, setTextTheme] = useState('black');
-  const [subTextTheme, setSubTextTheme] = useState('black');
+  const [lob, setLob] = useState("Light");
+  const [background, setBackground] = useState("#ffffff");
+  const [textTheme, setTextTheme] = useState("black");
+  const [subTextTheme, setSubTextTheme] = useState("black");
+
   const LightStyles = styled(Button)<ButtonProps>(() => ({
-    '&.MuiButton-root': {
-      border: '1px solid rgba(73, 22, 104, 1)',
+    "&.MuiButton-root": {
+      border: "1px solid rgba(73, 22, 104, 1)",
     },
-    '&.MuiButton-text': {
-      color: '#491668',
+    "&.MuiButton-text": {
+      color: "#491668",
     },
-    '&.MuiButton-outlined': {
-      color: '#491668',
+    "&.MuiButton-outlined": {
+      color: "#491668",
     },
   }));
 
   const DarkStyles = styled(Button)<ButtonProps>(() => ({
-    '&.MuiButton-root': {
-      border: '1px solid rgba(181, 108, 235, 1)',
+    "&.MuiButton-root": {
+      border: "1px solid rgba(181, 108, 235, 1)",
     },
-    '&.MuiButton-text': {
-      color: '#B56CEB',
+    "&.MuiButton-text": {
+      color: "#B56CEB",
     },
-    '&.MuiButton-outlined': {
-      color: '#B56CEB',
+    "&.MuiButton-outlined": {
+      color: "#B56CEB",
     },
   }));
 
   const themeHandler = () => {
     if (chartTheme === lightTheme) {
       setChartTheme(darkTheme);
-      console.log('Dark Theme config: ', darkTheme);
-      setLob('Dark');
-      setBackground('#2C2C2E');
-      setTextTheme('white');
-      setSubTextTheme('rgba(255, 255, 255, 0.5)');
-      return 'dark';
+      // console.log('Dark Theme config: ', darkTheme);
+      // setLob('Dark');
+      // setBackground('#2C2C2E');
+      // setTextTheme('white');
+      // setSubTextTheme('rgba(255, 255, 255, 0.5)');
+      return "dark";
     } else {
       setChartTheme(lightTheme);
-      console.log('Light Theme config: ', lightTheme);
-      setLob('Light');
-      setBackground('#ffffff');
-      setTextTheme('black');
-      setSubTextTheme('black');
+      // console.log('Light Theme config: ', lightTheme);
+      // setLob('Light');
+      // setBackground('#ffffff');
+      // setTextTheme('black');
+      // setSubTextTheme('black');
 
-      return 'light';
+      return "light";
     }
   };
 
