7ff9c7b7d957e7fdfb13060bcac81e7264ab8d72
Alex Cash <ac185426@ncr.com>
2022-03-14 11:31:46 -0400

    Added Scatter chart

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,16 @@ import CardComponent from './Components/BarCard';
 import GaugeCard from './Components/GaugeCard';
 import { Box } from '@mui/material';
 import { data3 } from './Data/data3';
-import { data2 } from './Data/data2';
 import { ScatterChart } from './Components/ScatterChart';
 
 function App() {
   const dataCardOne = data3();
-  const dataCardTwo = data2();
 
   return (
-    <Box sx={{ backgroundColor: '#EBEBF599', width: '100%', height: '100%' }}>
+    <Box sx={{ backgroundColor: '#EBEBF599', width: '100vw', height: '100vh' }}>
       <CardComponent />
       <Box sx={{ display: 'flex', width: '110em' }}>
         <GaugeCard percent={dataCardOne.percent} dataGauge={dataCardOne.dataGauge} />
-        {/* <GaugeCard percent={dataCardTwo.percent} dataGauge={dataCardTwo.dataGauge} /> */}
         <ScatterChart />
       </Box>
     </Box>
